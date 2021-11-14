a69bc11fb885a5c34012523328b2538baec3b2a3
kurshakuz <abilkasov@gmail.com>
2021-11-14 23:16:42 +0100

    restructure code

--- ECoL_Python.py
+++ ECoL_Python.py
@@ -4,10 +4,10 @@ from rpy2.robjects import pandas2ri
 from rpy2.robjects.conversion import localconverter
 
 r = robjects.r
-r['source']('ecol_complexity.r')
+r['source']('ECoL_complexity_functions.r')
 
 # load iris dataset
-pd_df = pd.read_csv("iris.csv")
+pd_df = pd.read_csv('./data/iris.csv')
 
 # data selection is done here
 # supply a list of rows that will be used
@@ -32,47 +32,47 @@ print(result_r)
 # available groups are:
 # overlapping, neighborhood, linearity, dimensionality, balance, network
 group_complexity_check_function_r = robjects.globalenv['group_complexity_check']
-result_r = group_complexity_check_function_r(r_from_pd_df, "overlapping")
+result_r = group_complexity_check_function_r(r_from_pd_df, 'overlapping')
 print(result_r)
 
 # compute feature based complexity for the given measure
 # available measures are:
 # F1, F1v, F2, F3, F4
 overlapping_complexity_check_function_r = robjects.globalenv['overlapping_complexity_check']
-result_r = overlapping_complexity_check_function_r(r_from_pd_df, "F1")
+result_r = overlapping_complexity_check_function_r(r_from_pd_df, 'F1')
 print(result_r)
 
 # compute neighborhood based complexity for the given measure
 # available measures are:
 # N1, N2, N3, N4, T1, LSC
 neighborhood_complexity_check_function_r = robjects.globalenv['neighborhood_complexity_check']
-result_r = neighborhood_complexity_check_function_r(r_from_pd_df, "N1")
+result_r = neighborhood_complexity_check_function_r(r_from_pd_df, 'N1')
 print(result_r)
 
 # compute linearity based complexity for the given measure
 # available measures are:
 # L1, L2, L3
 linearity_complexity_check_function_r = robjects.globalenv['linearity_complexity_check']
-result_r = linearity_complexity_check_function_r(r_from_pd_df, "L1")
+result_r = linearity_complexity_check_function_r(r_from_pd_df, 'L1')
 print(result_r)
 
 # compute dimensionality based complexity for the given measure
 # available measures are:
 # T2, T3, T4
 dimensionality_complexity_check_function_r = robjects.globalenv['dimensionality_complexity_check']
-result_r = dimensionality_complexity_check_function_r(r_from_pd_df, "T2")
+result_r = dimensionality_complexity_check_function_r(r_from_pd_df, 'T2')
 print(result_r)
 
 # compute balance based complexity for the given measure
 # available measures are:
 # C1, C2
 balance_complexity_check_function_r = robjects.globalenv['balance_complexity_check']
-result_r = balance_complexity_check_function_r(r_from_pd_df, "C1")
+result_r = balance_complexity_check_function_r(r_from_pd_df, 'C1')
 print(result_r)
 
 # compute network based complexity for the given measure
 # available measures are:
 # Density, ClsCoef”, Hubs
 network_complexity_check_function_r = robjects.globalenv['network_complexity_check']
-result_r = network_complexity_check_function_r(r_from_pd_df, "Density")
+result_r = network_complexity_check_function_r(r_from_pd_df, 'Density')
 print(result_r)
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This code provides a Python interface for [The Extended Complexity Library](https://github.com/lpfgarcia/ECoL).
 It can compute the specified data complexity measure for a provided data subset.
-Implementation details and use examples are provided in the `python_ecol.py` file.
+Implementation details and use examples are provided in the `ECoL_Python.py` file.
 
 ## Prerequisites
 
@@ -21,8 +21,8 @@ pip install -r requirements.txt
 
 ## Run the code
 
-Run the `python_ecol.py` file which will compute complexities for given complexity measure and a data subset.
+Run the `ECoL_Python.py` file which will compute complexities for given complexity measure and a data subset.
 
 ```
-python python_ecol.py
+python ECoL_Python.py
 ```

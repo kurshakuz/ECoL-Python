f233f5286adb139a65c4fccea3a5871f155db946
kurshakuz <abilkasov@gmail.com>
2021-11-17 21:32:49 +0100

    pass column selection to complexity measurement

--- ECoL_Python.py
+++ ECoL_Python.py
@@ -15,64 +15,79 @@ pd_df = pd.read_csv('./data/iris.csv')
 pd_df_indexed = pd_df.iloc[list(range(1, 150)), :]
 print(pd_df_indexed)
 
+# chose feature and class columns that will be
+# used for computing the complexities
+x_start_col = 0
+x_end_col = 4
+y_col = 5
+
+
 with localconverter(robjects.default_converter + pandas2ri.converter):
     r_from_pd_df = robjects.conversion.py2rpy(pd_df_indexed)
 
 # compute complexities for whole dataset
 all_complexities_check_function_r = robjects.globalenv['all_complexities_check']
-result_r = all_complexities_check_function_r()
+result_r = all_complexities_check_function_r(x_start_col, x_end_col, y_col)
 print(result_r)
 
 # compute complexities for dataset subset
 all_complexities_check_subset_function_r = robjects.globalenv['all_complexities_check_subset']
-result_r = all_complexities_check_subset_function_r(r_from_pd_df)
+result_r = all_complexities_check_subset_function_r(
+    r_from_pd_df, x_start_col, x_end_col, y_col)
 print(result_r)
 
 # compute complexities for the given complexity group
 # available groups are:
 # overlapping, neighborhood, linearity, dimensionality, balance, network
 group_complexity_check_function_r = robjects.globalenv['group_complexity_check']
-result_r = group_complexity_check_function_r(r_from_pd_df, 'overlapping')
+result_r = group_complexity_check_function_r(
+    r_from_pd_df, x_start_col, x_end_col, y_col, 'overlapping')
 print(result_r)
 
 # compute feature based complexity for the given measure
 # available measures are:
 # F1, F1v, F2, F3, F4
 overlapping_complexity_check_function_r = robjects.globalenv['overlapping_complexity_check']
-result_r = overlapping_complexity_check_function_r(r_from_pd_df, 'F1')
+result_r = overlapping_complexity_check_function_r(
+    r_from_pd_df, x_start_col, x_end_col, y_col, 'F1')
 print(result_r)
 
 # compute neighborhood based complexity for the given measure
 # available measures are:
 # N1, N2, N3, N4, T1, LSC
 neighborhood_complexity_check_function_r = robjects.globalenv['neighborhood_complexity_check']
-result_r = neighborhood_complexity_check_function_r(r_from_pd_df, 'N1')
+result_r = neighborhood_complexity_check_function_r(
+    r_from_pd_df, x_start_col, x_end_col, y_col, 'N1')
 print(result_r)
 
 # compute linearity based complexity for the given measure
 # available measures are:
 # L1, L2, L3
 linearity_complexity_check_function_r = robjects.globalenv['linearity_complexity_check']
-result_r = linearity_complexity_check_function_r(r_from_pd_df, 'L1')
+result_r = linearity_complexity_check_function_r(
+    r_from_pd_df, x_start_col, x_end_col, y_col, 'L1')
 print(result_r)
 
 # compute dimensionality based complexity for the given measure
 # available measures are:
 # T2, T3, T4
 dimensionality_complexity_check_function_r = robjects.globalenv['dimensionality_complexity_check']
-result_r = dimensionality_complexity_check_function_r(r_from_pd_df, 'T2')
+result_r = dimensionality_complexity_check_function_r(
+    r_from_pd_df, x_start_col, x_end_col, y_col, 'T2')
 print(result_r)
 
 # compute balance based complexity for the given measure
 # available measures are:
 # C1, C2
 balance_complexity_check_function_r = robjects.globalenv['balance_complexity_check']
-result_r = balance_complexity_check_function_r(r_from_pd_df, 'C1')
+result_r = balance_complexity_check_function_r(
+    r_from_pd_df, x_start_col, x_end_col, y_col, 'C1')
 print(result_r)
 
 # compute network based complexity for the given measure
 # available measures are:
 # Density, ClsCoef”, Hubs
 network_complexity_check_function_r = robjects.globalenv['network_complexity_check']
-result_r = network_complexity_check_function_r(r_from_pd_df, 'Density')
+result_r = network_complexity_check_function_r(
+    r_from_pd_df, x_start_col, x_end_col, y_col, 'Density')
 print(result_r)
--- ECoL_complexity_functions.r
+++ ECoL_complexity_functions.r
@@ -1,37 +1,37 @@
 library("ECoL")
 
-all_complexities_check <- function() {
-    return(complexity(iris[,1:4], iris[,5]))
+all_complexities_check <- function(x_start_col, x_end_col, y_col) {
+    return(complexity(iris[,x_start_col:x_end_col], iris[,y_col]))
 }
 
-all_complexities_check_subset <- function(input_dataframe) {
-    return(complexity(input_dataframe[,1:4], as.factor(input_dataframe[,5])))
+all_complexities_check_subset <- function(input_dataframe, x_start_col, x_end_col, y_col) {
+    return(complexity(input_dataframe[,x_start_col:x_end_col], as.factor(input_dataframe[,y_col])))
 }
 
-group_complexity_check <- function(input_dataframe, target_group) {
-    return(complexity(input_dataframe[,1:4], as.factor(input_dataframe[,5]), groups=target_group))
+group_complexity_check <- function(input_dataframe, x_start_col, x_end_col, y_col, target_group) {
+    return(complexity(input_dataframe[,x_start_col:x_end_col], as.factor(input_dataframe[,y_col]), groups=target_group))
 }
 
-overlapping_complexity_check <- function(input_dataframe, measure) {
-    return(overlapping(input_dataframe[,1:4], as.factor(input_dataframe[,5]), measures=measure))
+overlapping_complexity_check <- function(input_dataframe, x_start_col, x_end_col, y_col, measure) {
+    return(overlapping(input_dataframe[,x_start_col:x_end_col], as.factor(input_dataframe[,y_col]), measures=measure))
 }
 
-neighborhood_complexity_check <- function(input_dataframe, measure) {
-    return(neighborhood(input_dataframe[,1:4], as.factor(input_dataframe[,5]), measures=measure))
+neighborhood_complexity_check <- function(input_dataframe, x_start_col, x_end_col, y_col, measure) {
+    return(neighborhood(input_dataframe[,x_start_col:x_end_col], as.factor(input_dataframe[,y_col]), measures=measure))
 }
 
-linearity_complexity_check <- function(input_dataframe, measure) {
-    return(linearity(input_dataframe[,1:4], as.factor(input_dataframe[,5]), measures=measure))
+linearity_complexity_check <- function(input_dataframe, x_start_col, x_end_col, y_col, measure) {
+    return(linearity(input_dataframe[,x_start_col:x_end_col], as.factor(input_dataframe[,y_col]), measures=measure))
 }
 
-dimensionality_complexity_check <- function(input_dataframe, measure) {
-    return(dimensionality(input_dataframe[,1:4], as.factor(input_dataframe[,5]), measures=measure))
+dimensionality_complexity_check <- function(input_dataframe, x_start_col, x_end_col, y_col, measure) {
+    return(dimensionality(input_dataframe[,x_start_col:x_end_col], as.factor(input_dataframe[,y_col]), measures=measure))
 }
 
-balance_complexity_check <- function(input_dataframe, measure) {
-    return(balance(input_dataframe[,1:4], as.factor(input_dataframe[,5]), measures=measure))
+balance_complexity_check <- function(input_dataframe, x_start_col, x_end_col, y_col, measure) {
+    return(balance(input_dataframe[,x_start_col:x_end_col], as.factor(input_dataframe[,y_col]), measures=measure))
 }
 
-network_complexity_check <- function(input_dataframe, measure) {
-    return(network(input_dataframe[,1:4], as.factor(input_dataframe[,5]), measures=measure))
+network_complexity_check <- function(input_dataframe, x_start_col, x_end_col, y_col, measure) {
+    return(network(input_dataframe[,x_start_col:x_end_col], as.factor(input_dataframe[,y_col]), measures=measure))
 }
